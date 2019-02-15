caae8f07cfcbb8796865a481f893fdfcaee95b14
Poker <poker@inernetbs.net>
2019-02-15 15:20:38 +0200

    test apple hook

--- www/app/Http/Controllers/API/WebhookController.php
+++ www/app/Http/Controllers/API/WebhookController.php
@@ -3,16 +3,40 @@ namespace App\Http\Controllers\API;
 
 
 use App\Http\Controllers\Controller;
+use App\Services\SubscriptionsService;
 use Illuminate\Http\Request;
 
 class WebhookController extends Controller
 {
-    public function apple(Request $request, $applicationID)
+    public function apple(Request $request, $applicationID, SubscriptionsService $subscriptionsService)
     {
-        dump($applicationID);
-        dd($request->all());
 
 
+        \Log::info('REQUEST DATA', [
+            'data' => $request->all()
+        ]);
+
+
+
+
+//        $subscriptionsService->handlerReceipt(
+//            $subscription->application->app_id,
+//            $subscription->device_id,
+//            $subscription->screen_trial,
+//            $environment,
+//            $responseByAppleBody->latest_receipt,
+//            $responseByAppleBody->latest_receipt_info,
+//            $responseByAppleBody->pending_renewal_info[0]
+//        );
+
+
+//
+//        dump($applicationID);
+//        dd($request->all());
+
+        return [
+            'status' => 'OK'
+        ];
 
     }
 
